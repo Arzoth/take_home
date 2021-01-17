ded7796ebf30488cd0b4db178b62e494a481f6a2
Amaan Thakur <amaanthakur@amaans-mbp.attlocal.net>
2021-01-17 17:15:48 -0600

    add note

--- spec/features/shopping_card_spec.rb
+++ spec/features/shopping_card_spec.rb
@@ -7,12 +7,14 @@ feature 'shopping cart flow' do
 	let(:shopping_cart_page) { Pages::ShoppingCartPage.new}
 
 	before do 
+		#this may not work since we dont add this method to rails helper
 		resize_window_detault
 		shopping_cart_page.load
 	end 
    
    #this is for xs size scenario only 
 	scenario 'add XS product to the cart' do 
+	  visit 'https://react-shopping-cart-67954.firebaseapp.com/'
       shopping_cart_page.select_xs_size
       expect(shopping_cart_page).to have_content 'Cat Tee Black T-Shirt'
       expect(shopping_cart_page).not_to have_content 'Dark Thug Blue-Navy T-Shirt'
